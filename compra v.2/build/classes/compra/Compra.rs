1fbaa521ee350870ba48093d869ae5e0c916b5dc
Basito0 <76927670+Basito0@users.noreply.github.com>
2022-10-09 22:58:48 -0300

    Add files via upload
    Proyecto java con sources existentes (los de compra)

---
+++ compra v.2/build/classes/compra/Compra.rs
@@ -0,0 +1,14 @@
+compra.Pago
+compra.Cliente
+compra.Factura
+compra.Transferencia
+compra.Compra
+compra.Direccion
+compra.Boleta
+compra.Efectivo
+compra.DocTributario
+compra.Articulo
+compra.OrdenCompra
+compra.Tarjeta
+compra.DetalleOrden
+compra.Boleto
